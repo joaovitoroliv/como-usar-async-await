f7548f6f8ca86dd13400629a03c40f76eed7573e
Joao Souza <joao.souza@gerencianet.com.br>
2022-06-28 16:40:14 +0000

    update

--- example_4.js
+++ example_4.js
@@ -1,5 +1,8 @@
-function testeSimples(){ return 'da um like ai'; }
-console.log(testeSimples());
-async function testeSimplesAsync(){ return 'da um like ai'; }
-console.log(testeSimplesAsync());
-testeSimplesAsync().then((resposta) => console.log(resposta))
+import fetch from "node-fetch";
+async function testeSimplesAsync(){
+    const resultado = await fetch('https://api.github.com/users/joaovitoroliv')
+    console.log(resultado);
+}
+console.log('antes do async')
+testeSimplesAsync();
+console.log('da um likeclear ai async')
